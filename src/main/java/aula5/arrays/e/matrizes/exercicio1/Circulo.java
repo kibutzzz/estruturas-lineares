972d4a6d1e584ee7fef3de4761f43374235413b4
Leonardo Ramos <leonardo.ramos@sap.com>
2022-03-11 21:53:58 -0300

    add class for Circulo and its tests

---
+++ src/main/java/aula5/arrays/e/matrizes/exercicio1/Circulo.java
@@ -0,0 +1,42 @@
+package aula5.arrays.e.matrizes.exercicio1;
+
+public class Circulo {
+  private int x;
+  private int y;
+  private double raio;
+
+  public Circulo(int x, int y, double raio) {
+    this.x = x;
+    this.y = y;
+    this.raio = raio;
+  }
+
+  public double getArea() {
+    return Math.PI * raio * raio;
+  }
+
+  public double getCircunferencia() {
+    return 2 * Math.PI * raio;
+  }
+
+  public int getX() {
+    return x;
+  }
+
+  public int getY() {
+    return y;
+  }
+
+  public double getRaio() {
+    return raio;
+  }
+
+  @Override
+  public String toString() {
+    return "Circulo [" +
+        "x=" + x +
+        ", y=" + y +
+        ", raio=" + raio +
+        ']';
+  }
+}
---
+++ src/test/java/aula5/arrays/e/matrizes/exercicio1/CirculoTest.java
@@ -0,0 +1,42 @@
+package aula5.arrays.e.matrizes.exercicio1;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CirculoTest {
+
+  private static final double TOLERABLE_DIFFERENCE = 0.0001;
+
+  @Test
+  void shouldCreateObjectCorrectly() {
+    final var circulo = new Circulo(0, 1, 2);
+
+    assertEquals(0, circulo.getX());
+    assertEquals(1, circulo.getY());
+    assertEquals(2, circulo.getRaio());
+  }
+
+  @Test
+  void shouldCalculateAreaCorrectly() {
+    final var circulo = new Circulo(0, 0, 3);
+
+    assertEquals(28.2743, circulo.getArea(), TOLERABLE_DIFFERENCE);
+  }
+
+  @Test
+  void shouldCalculateCircumference() {
+    final var circulo = new Circulo(0, 0, 4);
+
+    assertEquals(25.1327, circulo.getCircunferencia(), TOLERABLE_DIFFERENCE);
+  }
+
+  @Test
+  void shouldConvertToStringCorrectly() {
+    final var circulo = new Circulo(1, 2, 3);
+
+    final var expected = "Circulo [x=1, y=2, raio=3.0]";
+    assertEquals(expected, circulo.toString());
+  }
+
+}
