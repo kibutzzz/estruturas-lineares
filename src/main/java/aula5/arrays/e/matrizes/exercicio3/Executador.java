5dbd2839ba2097291953a10683f024b8d2f5d0e7
Leonardo Ramos <leonardo.ramos@sap.com>
2022-03-13 02:28:52 -0300

    fix imports and indentation

--- src/main/java/aula5/arrays/e/matrizes/exercicio3/Executador.java
+++ src/main/java/aula5/arrays/e/matrizes/exercicio3/Executador.java
@@ -1,12 +1,10 @@
 package aula5.arrays.e.matrizes.exercicio3;
 
-import java.io.IOException;
-import java.nio.charset.StandardCharsets;
 import java.util.Scanner;
 
 public class Executador {
 
-  public static void main(String[] args) throws IOException {
+  public static void main(String[] args) {
     final var scanner = new Scanner(System.in);
 
     final var impressora = new ImpressoraTerminal();
