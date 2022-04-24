6ea5d9f99c7875991422a67e821b6fba46044822
Leonardo Ramos <leonardo.ramos@sap.com>
2022-04-24 09:51:50 -0300

    add Simple input to isolate the scanner dependency

---
+++ src/main/java/laboratorio2/helper/SimpleInput.java
@@ -0,0 +1,23 @@
+package laboratorio2.helper;
+
+import java.util.Scanner;
+
+//TODO Achar uma maneira de mocar o scanner para possibilitar testes
+public class SimpleInput {
+
+  private final Scanner scanner;
+
+  public SimpleInput(Scanner scanner) {
+    this.scanner = scanner;
+  }
+
+  public String lerLinha() {
+    return scanner.nextLine();
+  }
+
+  public int lerInteiro() {
+    final var inteiro = scanner.nextInt();
+    scanner.nextLine();
+    return inteiro;
+  }
+}
