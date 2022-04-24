1932e2234dcdea7c67e90ac122bc1a25770ba1aa
Leonardo Ramos <leonardo.ramos@sap.com>
2022-04-24 13:15:51 -0300

    implement mapa and tests

--- src/main/java/laboratorio2/Mapa.java
+++ src/main/java/laboratorio2/Mapa.java
@@ -1,10 +1,46 @@
 package laboratorio2;
 
+import java.util.Optional;
+import laboratorio2.helper.SimpleLogger;
+
 public class Mapa {
 
     private final Assento[][] assentos;
+    private final SimpleLogger logger;
 
-    public Mapa(int i) {
-        assentos = new Assento[i][i];
+    public Mapa(SimpleLogger logger, int tamanho) {
+        if (tamanho <= 0) {
+            throw new IllegalArgumentException("Tamanho deve ser maior que 0");
+        }
+        assentos = new Assento[tamanho][tamanho];
+        for (int fileira = 0; fileira < tamanho; fileira++) {
+            for (int coluna = 0; coluna < tamanho; coluna++) {
+                assentos[fileira][coluna] = new Assento();
+            }
+        }
+        this.logger = logger;
+    }
+
+    public void reservar(String nome, int fileira, int coluna) {
+        try {
+            final var assento = assentos[fileira][coluna];
+            assento.reservar(nome);
+        } catch (ArrayIndexOutOfBoundsException exception) {
+            throw new IllegalArgumentException("Posição selecionada não é valida");
+        }
+    }
+
+    public void cancelar(int fileira, int coluna) {
+        assentos[fileira][coluna] = new Assento();
+    }
+
+    public void mostrar() {
+        for (final var fileira : assentos) {
+            for (final var assento : fileira) {
+                final var nome = Optional.ofNullable(assento.getNome()).orElse("");
+                logger.imprimir(String.format("[%12s]\t", nome));
+            }
+            logger.imprimirLinha("");
+        }
     }
 }
---
+++ src/test/java/laboratorio2/MapaTest.java
@@ -0,0 +1,79 @@
+package laboratorio2;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.BDDMockito.then;
+import static org.mockito.Mockito.inOrder;
+import static org.mockito.Mockito.times;
+
+import laboratorio2.helper.SimpleLogger;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+@ExtendWith(MockitoExtension.class)
+class MapaTest {
+
+    @Mock private SimpleLogger logger;
+    private Mapa mapa;
+
+    @BeforeEach
+    void setup() {
+        mapa = new Mapa(logger, 2);
+    }
+
+    @Test
+    void shouldShowEmptyMapCorrectly() {
+        mapa.mostrar();
+
+        final var order = inOrder(logger);
+        then(logger).should(order, times(2)).imprimir("[            ]\t");
+        then(logger).should(order).imprimirLinha("");
+        then(logger).should(order, times(2)).imprimir("[            ]\t");
+    }
+
+    @Test
+    void shouldThrowExceptionWhenSizeIsInvalid() {
+        final var exception =
+                assertThrows(IllegalArgumentException.class, () -> new Mapa(logger, 0));
+
+        assertEquals("Tamanho deve ser maior que 0", exception.getMessage());
+    }
+
+    @Test
+    void shouldBeAbleToReserve() {
+        mapa.reservar("Bianca", 1, 1);
+        mapa.reservar("Leonardo", 0, 0);
+
+        mapa.mostrar();
+
+        final var order = inOrder(logger);
+        then(logger).should(order).imprimir("[    Leonardo]\t");
+        then(logger).should(order).imprimir("[            ]\t");
+        then(logger).should(order).imprimirLinha("");
+        then(logger).should(order).imprimir("[            ]\t");
+        then(logger).should(order).imprimir("[      Bianca]\t");
+    }
+
+    @Test
+    void shouldBeAbleToCancel() {
+        mapa.mostrar();
+        mapa.reservar("Reserva 1", 0, 0);
+        mapa.cancelar(0, 0);
+
+        final var order = inOrder(logger);
+        then(logger).should(order, times(2)).imprimir("[            ]\t");
+        then(logger).should(order).imprimirLinha("");
+        then(logger).should(order, times(2)).imprimir("[            ]\t");
+    }
+
+    @Test
+    void shouldHandleInvalidPosition() {
+        final var exception =
+                assertThrows(IllegalArgumentException.class, () -> mapa.reservar("bla", 0, 3));
+
+        assertEquals("Posição selecionada não é valida", exception.getMessage());
+    }
+}
