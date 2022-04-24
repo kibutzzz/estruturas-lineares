0aa166b43e44eae3dd866a9dcd04747e39ac35fc
Leonardo Ramos <leonardo.ramos@sap.com>
2022-04-24 11:06:32 -0300

    format

--- src/main/java/laboratorio2/helper/SimpleInput.java
+++ src/main/java/laboratorio2/helper/SimpleInput.java
@@ -2,22 +2,22 @@ package laboratorio2.helper;
 
 import java.util.Scanner;
 
-//TODO Achar uma maneira de mocar o scanner para possibilitar testes
+// TODO Achar uma maneira de mocar o scanner para possibilitar testes
 public class SimpleInput {
 
-  private final Scanner scanner;
+    private final Scanner scanner;
 
-  public SimpleInput(Scanner scanner) {
-    this.scanner = scanner;
-  }
+    public SimpleInput(Scanner scanner) {
+        this.scanner = scanner;
+    }
 
-  public String lerLinha() {
-    return scanner.nextLine();
-  }
+    public String lerLinha() {
+        return scanner.nextLine();
+    }
 
-  public int lerInteiro() {
-    final var inteiro = scanner.nextInt();
-    scanner.nextLine();
-    return inteiro;
-  }
+    public int lerInteiro() {
+        final var inteiro = scanner.nextInt();
+        scanner.nextLine();
+        return inteiro;
+    }
 }
