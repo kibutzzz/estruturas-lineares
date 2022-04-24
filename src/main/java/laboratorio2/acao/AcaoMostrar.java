24d0209cb321ed7292e5289fdb5dec1222dd5738
Leonardo Ramos <leonardo.ramos@sap.com>
2022-04-24 09:05:00 -0300

    implement show action

--- src/main/java/laboratorio2/acao/AcaoMostrar.java
+++ src/main/java/laboratorio2/acao/AcaoMostrar.java
@@ -2,12 +2,21 @@ package laboratorio2.acao;
 
 import java.util.function.Consumer;
 import laboratorio2.Mapa;
+import laboratorio2.helper.SimpleLogger;
 
 public class AcaoMostrar implements Consumer<Mapa> {
 
+    private static final String TITULO = "Assentos da sala:";
+
+    private final SimpleLogger logger;
+
+    public AcaoMostrar(SimpleLogger logger) {
+        this.logger = logger;
+    }
+
     @Override
     public void accept(Mapa mapa) {
-        System.out.println("Mapa:");
+        logger.imprimirLinha(TITULO);
         mapa.mostrar();
     }
 }
--- src/test/java/laboratorio2/acao/AcaoMostrarTest.java
+++ src/test/java/laboratorio2/acao/AcaoMostrarTest.java
@@ -3,10 +3,12 @@ package laboratorio2.acao;
 import static org.mockito.BDDMockito.then;
 
 import laboratorio2.Mapa;
+import laboratorio2.helper.SimpleLogger;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
+import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
 
 @ExtendWith(MockitoExtension.class)
@@ -14,11 +16,15 @@ class AcaoMostrarTest {
 
     @InjectMocks private AcaoMostrar acaoMostrar;
     @Mock private Mapa mapa;
+    @Mock private SimpleLogger logger;
 
     @Test
     void shouldDelegateToMapa() {
         acaoMostrar.accept(mapa);
 
-        then(mapa).should().mostrar();
+        final var order = Mockito.inOrder(logger, mapa);
+
+        then(logger).should(order).imprimirLinha("Assentos da sala:");
+        then(mapa).should(order).mostrar();
     }
 }
