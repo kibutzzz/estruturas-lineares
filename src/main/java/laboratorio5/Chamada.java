b552c0a46e4ffdbd6a1912b6f900427622352b65
Leonardo Ramos <leonardo.ramos@sap.com>
2022-06-17 11:54:24 -0300

    formatting

--- src/main/java/laboratorio5/Chamada.java
+++ src/main/java/laboratorio5/Chamada.java
@@ -3,31 +3,28 @@ package laboratorio5;
 import java.util.Objects;
 
 public class Chamada {
-	
-	private String numero;
-	
-	public Chamada(String numero) {
-		this.numero = numero;
-	}
-	
-	public String getNumero() {
-		return this.numero;
-	}
-	
-	@Override
-	 public String toString() {
-	   return "Número: " + getNumero();
-	 }
 
-	@Override
-	public boolean equals(Object obj) {
-		if (this == obj)
-			return true;
-		if (obj == null)
-			return false;
-		if (getClass() != obj.getClass())
-			return false;
-		Chamada other = (Chamada) obj;
-		return Objects.equals(numero, other.numero);
-	}  
+    private String numero;
+
+    public Chamada(String numero) {
+        this.numero = numero;
+    }
+
+    public String getNumero() {
+        return this.numero;
+    }
+
+    @Override
+    public String toString() {
+        return "Número: " + getNumero();
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) return true;
+        if (obj == null) return false;
+        if (getClass() != obj.getClass()) return false;
+        Chamada other = (Chamada) obj;
+        return Objects.equals(numero, other.numero);
+    }
 }
--- src/main/java/laboratorio5/Telefone.java
+++ src/main/java/laboratorio5/Telefone.java
@@ -4,68 +4,68 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 import java.util.Scanner;
-import java.util.stream.Collectors;
 import java.util.function.Function;
+import java.util.stream.Collectors;
 
 public class Telefone {
 
-	private List<Contato> contatos = new ArrayList<>();
-	private List<Chamada> chamadas = new ArrayList<>();
+    private List<Contato> contatos = new ArrayList<>();
+    private List<Chamada> chamadas = new ArrayList<>();
 
-	Scanner menu = new Scanner(System.in);
+    Scanner menu = new Scanner(System.in);
 
-	public void adicionarContato(String nome, String numero) {
-		contatos.add(new Contato(nome, numero));
-	}
+    public void adicionarContato(String nome, String numero) {
+        contatos.add(new Contato(nome, numero));
+    }
 
-	public void removerContato(String numero) {
-		final var contatoASerRemovido = buscarContatoPorNumero(numero).orElseThrow();
-		contatos.remove(contatoASerRemovido);
-	}
-	
-	public String buscarNomePorNumero(String numero) {
-		return buscarContatoPorNumero(numero).map(Contato::getNome).orElseThrow();
-	}
-	
-	private Optional<Contato> buscarContatoPorNumero(String numero){
-		return contatos.stream().filter(contato -> contato.getNumero().equals(numero)).findFirst();
-	}
-	
-	public void adicionarChamadaNaoAtendida(String numero) {
-		chamadas.add(new Chamada(numero));
-	}
-	
-	public List<Contato> mostrarChamadasNaoAtendidas() {
-		return chamadas.stream().map(paraContato(contatos)).collect(Collectors.toList());
-	}
-	
-	private static Function<Chamada, Contato> paraContato(List<Contato> contatos) {
-		return chamada -> contatos.stream()
-				.filter(contato -> contato.getNumero().equals(chamada.getNumero()))
-				.findFirst().orElseGet(() -> new Contato("Número desconhecido", chamada.getNumero()));
-	}
-	
-	public void removerChamadaNaoAtendida() {
-		chamadas.clear();
-	}
-	
-	public List<Contato> getContatos(){
-		return contatos;
-	}
-	
-	public List<Chamada> getChamadas(){
-		return chamadas;
-	}
+    public void removerContato(String numero) {
+        final var contatoASerRemovido = buscarContatoPorNumero(numero).orElseThrow();
+        contatos.remove(contatoASerRemovido);
+    }
 
+    public String buscarNomePorNumero(String numero) {
+        return buscarContatoPorNumero(numero).map(Contato::getNome).orElseThrow();
+    }
+
+    private Optional<Contato> buscarContatoPorNumero(String numero) {
+        return contatos.stream().filter(contato -> contato.getNumero().equals(numero)).findFirst();
+    }
+
+    public void adicionarChamadaNaoAtendida(String numero) {
+        chamadas.add(new Chamada(numero));
+    }
+
+    public List<Contato> mostrarChamadasNaoAtendidas() {
+        return chamadas.stream().map(paraContato(contatos)).collect(Collectors.toList());
+    }
+
+    private static Function<Chamada, Contato> paraContato(List<Contato> contatos) {
+        return chamada ->
+                contatos.stream()
+                        .filter(contato -> contato.getNumero().equals(chamada.getNumero()))
+                        .findFirst()
+                        .orElseGet(() -> new Contato("Número desconhecido", chamada.getNumero()));
+    }
+
+    public void removerChamadaNaoAtendida() {
+        chamadas.clear();
+    }
+
+    public List<Contato> getContatos() {
+        return contatos;
+    }
+
+    public List<Chamada> getChamadas() {
+        return chamadas;
+    }
 }
 
-
-//try {
+// try {
 //
 //	final var contatoRemovido = contatos.stream()
 //			.filter(contato -> contato.getNumero().equals(numero)).findFirst().orElseThrow();
 //
-//} catch (NoSuchElementException e) {
+// } catch (NoSuchElementException e) {
 //	System.out.println("Contato não encontrado.");
-//}
-//}
+// }
+// }
