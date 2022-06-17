99f05350ba48e859afc16fb20f7e72b86f4d77b0
Leonardo Ramos <leonardo.ramos@sap.com>
2022-06-17 14:49:23 -0300

    formmating

--- src/main/java/controledeestoque/Produto.java
+++ src/main/java/controledeestoque/Produto.java
@@ -2,35 +2,35 @@ package controledeestoque;
 
 public class Produto {
 
-  private String codigo;
-  private String nome;
-  private int quantidade;
-  private int quantidadeMinima;
+    private String codigo;
+    private String nome;
+    private int quantidade;
+    private int quantidadeMinima;
 
-  public Produto(String codigo, String nome, int quantidade, int quantidadeMinima) {
-    this.codigo = codigo;
-    this.nome = nome;
-    this.quantidade = quantidade;
-    this.quantidadeMinima = quantidadeMinima;
-  }
+    public Produto(String codigo, String nome, int quantidade, int quantidadeMinima) {
+        this.codigo = codigo;
+        this.nome = nome;
+        this.quantidade = quantidade;
+        this.quantidadeMinima = quantidadeMinima;
+    }
 
-  public void alterarQuantidade(int novaQuantidade) {
-    this.quantidade = novaQuantidade;
-  }
+    public void alterarQuantidade(int novaQuantidade) {
+        this.quantidade = novaQuantidade;
+    }
 
-  public String getNome() {
-    return nome;
-  }
+    public String getNome() {
+        return nome;
+    }
 
-  public String getCodigo() {
-    return codigo;
-  }
+    public String getCodigo() {
+        return codigo;
+    }
 
-  public int getQuantidade() {
-    return quantidade;
-  }
+    public int getQuantidade() {
+        return quantidade;
+    }
 
-  public int getQuantidadeMinima() {
-    return quantidadeMinima;
-  }
+    public int getQuantidadeMinima() {
+        return quantidadeMinima;
+    }
 }
--- src/test/java/controledeestoque/EstoqueTest.java
+++ src/test/java/controledeestoque/EstoqueTest.java
@@ -1,81 +1,82 @@
 package controledeestoque;
 
-import org.junit.jupiter.api.Test;
-
-import java.util.Collections;
-import java.util.List;
-
 import static java.util.Collections.emptyList;
 import static org.junit.jupiter.api.Assertions.*;
 
+import java.util.List;
+import org.junit.jupiter.api.Test;
+
 class EstoqueTest {
 
-  private Estoque estoque = new Estoque();
+    private Estoque estoque = new Estoque();
 
-  @Test
-  void naoDeveAdicionarProdutosNulo() {
-    final var exception = assertThrows(IllegalArgumentException.class, () -> estoque.adicionarProduto(null));
+    @Test
+    void naoDeveAdicionarProdutosNulo() {
+        final var exception =
+                assertThrows(IllegalArgumentException.class, () -> estoque.adicionarProduto(null));
 
-    assertEquals("Produto não pode ser nulo", exception.getMessage());
-    assertEquals(emptyList(), estoque.listarProdutos());
-  }
+        assertEquals("Produto não pode ser nulo", exception.getMessage());
+        assertEquals(emptyList(), estoque.listarProdutos());
+    }
 
-  @Test
-  void naoDeveAdicionarProdutoComCodigoRepetido() {
-    final var batata = new Produto("1", "Batata", 10, 10);
-    final var feijao = new Produto("1", "Feijão", 20, 10);
+    @Test
+    void naoDeveAdicionarProdutoComCodigoRepetido() {
+        final var batata = new Produto("1", "Batata", 10, 10);
+        final var feijao = new Produto("1", "Feijão", 20, 10);
 
-    estoque.adicionarProduto(batata);
-    final var exception = assertThrows(IllegalArgumentException.class, () -> estoque.adicionarProduto(feijao));
+        estoque.adicionarProduto(batata);
+        final var exception =
+                assertThrows(
+                        IllegalArgumentException.class, () -> estoque.adicionarProduto(feijao));
 
-    assertEquals("Já existe um produto cadastrado com este código", exception.getMessage());
-    assertEquals(List.of(batata), estoque.listarProdutos());
-  }
+        assertEquals("Já existe um produto cadastrado com este código", exception.getMessage());
+        assertEquals(List.of(batata), estoque.listarProdutos());
+    }
 
-  @Test
-  void naoDevePoderAlterarQuantidadeParaMenosQueZero() {
-    final var batata = new Produto("1", "Batata", 10, 10);
+    @Test
+    void naoDevePoderAlterarQuantidadeParaMenosQueZero() {
+        final var batata = new Produto("1", "Batata", 10, 10);
 
-    estoque.adicionarProduto(batata);
-    final var exception = assertThrows(IllegalArgumentException.class, () -> estoque.alterarQuantidadeDeProduto("1", -1));
+        estoque.adicionarProduto(batata);
+        final var exception =
+                assertThrows(
+                        IllegalArgumentException.class,
+                        () -> estoque.alterarQuantidadeDeProduto("1", -1));
 
-    assertEquals("Não é possivel alterar quantidade para -1", exception.getMessage());
-    assertEquals(10, batata.getQuantidade());
-  }
+        assertEquals("Não é possivel alterar quantidade para -1", exception.getMessage());
+        assertEquals(10, batata.getQuantidade());
+    }
 
-  @Test
-  void devePoderAlterarQuantidadeParaZero() {
-    final var batata = new Produto("1", "Batata", 10, 10);
+    @Test
+    void devePoderAlterarQuantidadeParaZero() {
+        final var batata = new Produto("1", "Batata", 10, 10);
 
-    estoque.adicionarProduto(batata);
-    estoque.alterarQuantidadeDeProduto("1", 0);
+        estoque.adicionarProduto(batata);
+        estoque.alterarQuantidadeDeProduto("1", 0);
 
-    assertEquals(0, batata.getQuantidade());
-  }
+        assertEquals(0, batata.getQuantidade());
+    }
 
-  @Test
-  void devePoderAlterarQuantidadeParaMaisQueZero() {
-     var batata = new Produto("1", "Batata", 10, 10);
+    @Test
+    void devePoderAlterarQuantidadeParaMaisQueZero() {
+        var batata = new Produto("1", "Batata", 10, 10);
 
-      estoque.adicionarProduto(batata);
-      estoque.alterarQuantidadeDeProduto("1", 1);
+        estoque.adicionarProduto(batata);
+        estoque.alterarQuantidadeDeProduto("1", 1);
 
-      assertEquals(1, batata.getQuantidade());
-  }
+        assertEquals(1, batata.getQuantidade());
+    }
 
-  @Test
-  void deveListarApenasProdutosAbaixoDoMinimo() {
-    final var batata = new Produto("1", "Batata", 10, 10);
-    final var arroz = new Produto("2", "Arroz", 5, 10);
-    final var feijao = new Produto("112", "Feijão", 20, 10);
+    @Test
+    void deveListarApenasProdutosAbaixoDoMinimo() {
+        final var batata = new Produto("1", "Batata", 10, 10);
+        final var arroz = new Produto("2", "Arroz", 5, 10);
+        final var feijao = new Produto("112", "Feijão", 20, 10);
 
-    estoque.adicionarProduto(batata);
-    estoque.adicionarProduto(arroz);
-    estoque.adicionarProduto(feijao);
+        estoque.adicionarProduto(batata);
+        estoque.adicionarProduto(arroz);
+        estoque.adicionarProduto(feijao);
 
-    assertEquals(List.of(arroz), estoque.listarProdutosAbaixoMinimo());
-  }
-
-
-
+        assertEquals(List.of(arroz), estoque.listarProdutosAbaixoMinimo());
+    }
 }
